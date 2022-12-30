8215b52898b541f5eb30b1b0c6fd00cc48f24803
firsttoken <monikasharma12330@gmail.com>
2022-12-30 21:45:47 +0530

    Model_Class

---
+++ Details.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventryManagement
+{
+    public class Details
+    {
+        public List<TypeOfRice> typeOfRice;//its a variable of typeofrice classname datatype
+        public List<TypeOfWheat> typeOfWheat;//its a variable of typeofwheat classnamedatatype
+        public List<TypeOfPulse> typeOfPulse;//its a variable of typeofpulse classnamedatatype
+    }
+    public class TypeOfRice
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypeOfWheat
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypeOfPulse
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+}
