b654122c7ba1ac668af461675da648e25990afe4
firsttoken <monikasharma12330@gmail.com>
2022-12-30 21:47:30 +0530

    fatching data from json file

---
+++ FatchInventryDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace InventryManagement
+{
+    public class FatchInventryDetails
+    {
+        public Details Read(String path)//method detail datatype method name is read
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    String json=file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Details>(json);
+
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+    }
+}
